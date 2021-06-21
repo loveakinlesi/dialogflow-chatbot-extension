265057eb85f5736a9e25597893a6cd93e7f123b8
iiitma <akinlesil@gmail.com>
2021-06-21 01:26:03 +0100

    Update realtimeDatabase.js

--- libs/realtimeDatabase.js
+++ libs/realtimeDatabase.js
@@ -1,84 +1,157 @@
 const admin = require("firebase-admin");
+const { v1: uuidv1 } = require("uuid");
 module.exports = class realtimeDatabase {
-    
-    #session = {
-        step: '',
-        sections: '',
-        currentsection: '',
-        topics: '',
-        currenttopic: '',
-        tour: '',
-        name: '',
-    }
-
-    constructor(object) {
+  #timeZone
+    constructor(credentials, databaseURL, timeZone) {
         admin.initializeApp({
-            credential: admin.credential.cert(object.credentials),
-            databaseURL: object.databaseURL,
+            credential: admin.credential.cert(credentials),
+            databaseURL: databaseURL,
           });
+          this.#timeZone = timeZone
         this.admin = admin;
         this.db = admin.database();
       }
 
+      async authorize({email, ipaddress, deviceDetails, location}){
+        const user = await this.#getSessionByEmail(email);
+        if (user){
+          return {
+            message: "user found",
+            sessionId: user.sessionId,
+          }
+        } else{
+          return this.#createUserSession({email, ipaddress, deviceDetails, location})
+  
+        }
+      }
 
-      savequery(sessionId, res) {
+      saveQuery(sessionId, res) {
         let data = {
           sessionId: sessionId,
           query: res.queryResult.queryText,
-          dateTime: new Date().toDateString(),
+          dateTime: new Date().toLocaleString('en-US', {
+            timeZone: this.#timeZone
+          }),
           action: res.queryResult.action,
           responseName: res.queryResult.intent.displayName,
         }
-      admin.database().ref("queryList").push(data)
+        this.db.ref("queryList").push(data)
     }
 
 
-    getsessionkey(sessionId) {
-        var db = admin.database();
-        var sessions = db.ref("sessionkey");
-        return sessions.on("value", (snapshot) => {
-          var listofkeys = snapshot.val();
-          for (const i in listofkeys) {
-            if (listofkeys[i].sessionId == sessionId) {
-              return listofkeys[i].key;
-            }
-          }
-        });
+
+
+    getRealtimeData(tableName){
+      return this.#getDataExtension(tableName);
+    }
+
+    async getSessionKey(sessionId) {
+      const snapshot = await this.#getDataExtension("sessionkey");
+      for (const i in snapshot) {
+        if (snapshot[i].sessionId == sessionId) {
+            return snapshot[i].key;
+        }
       }
+    }
   
-      async getsession(sessionId) {
-        var db = admin.database();
-        var usersSession = db.ref("session");
-        let session = await usersSession.on("value", (snapshot) => {
-          var listofsessions = snapshot.val();
-          for (const i in listofsessions) {
-            if (listofsessions[i].sessionId == sessionId) {
-              step = listofsessions[i].step;
-              sections = listofsessions[i].sections;
-              currentsection = listofsessions[i].currentsection;
-              topics = listofsessions[i].topics;
-              currenttopic = listofsessions[i].currenttopic;
-              tour = listofsessions[i].tour;
-              name = listofsessions[i].name;
-            }
+    async getSession(sessionId) {
+
+      const snapshot = await this.#getDataExtension("session");
+        for (const i in snapshot) {
+          if (snapshot[i].sessionId == sessionId) {
+            return new Session(snapshot[i])
           }
-        });
+        }
+    }
+
+    async saveSession(sessionId, session) {
+      const sessionKey =  await this.getSessionKey(sessionId);
+      let userSession = this.db.ref("session/" + sessionKey);
+  
+      userSession.update(session);
+      console.log('\x1b[36m%s\x1b[0m', `Session saved! - {${sessionId}}`) 
       }
-  
-    //    savesession() {
-    //     getsessionkey(sessionId);
-    //     var key = sessionkey;
-    //     var db = admin.database();
-    //     var user = db.ref("session/" + key);
-  
-    //     user.update({
-    //       sessionId: sessionId,
-    //       step: step,
-    //       sections: sections,
-    //       currentsection: currentsection,
-    //       topics: topics,
-    //       currenttopic: currenttopic,
-    //       tour: tour,
-    //     });
-    //   }
+
+
+
+    async #getDataExtension(tableName){
+      const tableData = await this.db.ref(tableName).once("value", (snapshot) => {
+          let data = snapshot.val();
+          return data;
+      })
+      return tableData.val();
+    }
+
+    async #getSessionByEmail(email){
+      const snapshot = await this.#getDataExtension("sessionkey");
+      for (const i in snapshot) {
+        if (snapshot[i].email == email) {
+            return snapshot[i];
+        }
+      }
+      return null
+    }
+
+
+    async #createUserSession({email, ipaddress, deviceDetails, location, name=''}){
+      const sessionId = uuidv1();
+
+      var users = this.db.ref("session");
+      var newuser = users.push();
+      newuser.set(new Session({
+        sessionId: sessionId,
+        step: 0,
+        sections: ["introduction", "worktools", "policies"],
+        currentsection: "",
+        topics: "",
+        currenttopic: "",
+        tour: "pending",
+        name: name,
+        email: email,
+      }));
+      var postId = newuser.key;
+      this.db.ref("sessionkey").push({
+        sessionId: sessionId,
+        key: postId,
+        email: email,
+        ipaddress: ipaddress,
+        deviceDetails: deviceDetails,
+        location: location,
+        dataJoined: new Date().toLocaleString('en-US', {
+          timeZone: this.#timeZone
+        }),
+      });
+     
+      return {
+        message: "user created",
+        sessionId: sessionId,
+      }
+
+    }
+
+
 }
+
+
+class Session {
+  constructor(data){
+    this.step = data.step;
+    this.sections = data.sections;
+    this.currentsection = data.currentsection;
+    this.topics = data.topics;
+    this.currenttopic = data.currenttopic;
+    this.sessionId = data.sessionId
+    this.tour = data.tour;
+    this.name = data.name;
+    return this
+  }
+}
+
+
+
+
+//  authorize 
+// {name, email, sessionId, ipAddress, location}
+// get sessionId by email
+//     if exists return sessionId
+//     else create user and return sessionId
